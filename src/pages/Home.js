8d29aa641822231615e6b499c33956e31ee4a81e
Vladimir Bredikhin <brutal.v4@gmail.com>
2020-02-21 19:34:00 +0200

    Add navigation

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = props => (
+  <div className="">
+    <h1>Home page</h1>
+  </div>
+);
+
+export default Home;
---
+++ src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Profile = props => (
+  <div className="">
+    <h1>Profile page</h1>
+  </div>
+);
+
+export default Profile;
