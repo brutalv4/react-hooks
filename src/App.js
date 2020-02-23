2580e0d74bb1383cda734a869edc148f00559fba
Vladimir Bredikhin <brutal.v4@gmail.com>
2020-02-23 22:29:01 +0200

    Update Github state

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import { AlertState } from './context/alert/AlertState';
+import { GithubState } from './context/github/GithubState';
 import About from './pages/About';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 
 function App() {
   return (
-    <AlertState>
-      <BrowserRouter>
-        <Navbar />
-        <div className="container pt-4">
-          <Alert alert={{ text: 'Test alert' }} />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/profile/:name" component={Profile} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </AlertState>
+    <GithubState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert alert={{ text: 'Test alert' }} />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/profile/:name" component={Profile} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </GithubState>
   );
 }
 
